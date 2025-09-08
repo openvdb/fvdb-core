1074417a9cc599433db141552f3c11c6a8508351
Jonathan Swartz <jonathan@jswartz.info>
2025-09-09 04:06:38 +1200

    CMake modifications to vendor in NanoVDB
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

---
+++ src/cmake/get_nanovdb.cmake
@@ -0,0 +1,17 @@
+# Copyright Contributors to the OpenVDB Project
+# SPDX-License-Identifier: Apache-2.0
+
+CPMAddPackage(
+    NAME nanovdb
+    GIT_REPOSITORY git@github.com:matthewdcong/openvdb-aswf.git
+    GIT_TAG 7bb1699a7db6830b50a66befcc245ae4e74455ed
+    SOURCE_SUBDIR nanovdb/nanovdb
+    DOWNLOAD_ONLY YES
+)
+
+# NanoVDB is header only, so we don't build it. Instead, we just add the headers
+# to the include path and create an interface target.
+if(nanovdb_ADDED)
+    add_library(nanovdb INTERFACE)
+    target_include_directories(nanovdb INTERFACE ${nanovdb_SOURCE_DIR}/nanovdb)
+endif()
