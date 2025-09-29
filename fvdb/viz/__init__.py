362c19b8514e1d6880f6ff426433d63aeeecba6b
Francis Williams <francis@fwilliams.info>
2025-09-29 15:47:38 -0700

    rename
    Signed-off-by: Francis Williams <francis@fwilliams.info>

--- fvdb/viz/__init__.py
+++ fvdb/viz/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 from ._utils import grid_edge_network, gridbatch_edge_network
-from .viewer import CameraView, GaussianSplat3dView, Viewer
+from ._viewer import CameraView, GaussianSplat3dView, Viewer
 
 __all__ = [
     "Viewer",
