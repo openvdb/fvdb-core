abf2674fc50cbb42d176adf5586c6903b3ebc093
Mark Harris <mharris@nvidia.com>
2025-10-08 08:39:12 +1100

    Only one + allowed in PEP440 naming
    Signed-off-by: Mark Harris <mharris@nvidia.com>

--- scripts/rename_wheels.py
+++ scripts/rename_wheels.py
@@ -11,12 +11,23 @@ torch_version = sys.argv[1]
 cuda_version = sys.argv[2]
 wheel_glob = sys.argv[3] if len(sys.argv) > 3 else "dist/*.whl"
 
+"""
+Utility to append a PEP 440 local version to built wheel filenames for offline distribution.
+
+Note: This only renames files on disk and does not modify the wheel's embedded METADATA
+version. Do not use this for uploads to PyPI/TestPyPI, as the filename and METADATA
+versions must match for a valid upload.
+"""
+
 wheels = glob.glob(wheel_glob)
 for wheel in wheels:
     wheel = os.path.basename(wheel)
     filename, ext = os.path.splitext(wheel)
     tags = filename.split("-")
-    new_filename = "-".join(tags[:-4] + [tags[-4] + "+" + "torch" + torch_version + "+" + cuda_version] + tags[-3:])
-    new_filename += ext
+    # tags layout: [name, version, build? (optional), pyTag, abiTag, platTag]
+    # We expect version at index -4 for standard wheels without build tag
+    local_tag = "torch" + torch_version + "." + cuda_version
+    new_version = tags[-4] + "+" + local_tag
+    new_filename = "-".join(tags[:-4] + [new_version] + tags[-3:]) + ext
     print(f"Renaming {wheel} -> {new_filename}")
     os.rename(os.path.join("dist", wheel), os.path.join("dist", new_filename))
