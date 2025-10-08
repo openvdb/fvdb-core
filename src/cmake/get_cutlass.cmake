ab4d9547bbbe834546ff747abb8f38b20da0b1e9
Matthew Cong <mcong@nvidia.com>
2025-10-08 12:58:12 -0700

    Update CUTLASS from v3.8.0 to v4.2.1
    Signed-off-by: Matthew Cong <mcong@nvidia.com>

--- src/cmake/get_cutlass.cmake
+++ src/cmake/get_cutlass.cmake
@@ -5,7 +5,7 @@ CPMAddPackage(
     NAME cutlass
     GITHUB_REPOSITORY NVIDIA/cutlass
     PATCH_COMMAND git apply --ignore-space-change --ignore-whitespace ${CMAKE_CURRENT_SOURCE_DIR}/../env/cutlass.patch || exit 0
-    GIT_TAG v3.8.0
+    GIT_TAG v4.2.1
     DOWNLOAD_ONLY YES
 )
 
