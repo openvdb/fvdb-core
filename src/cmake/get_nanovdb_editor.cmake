e1b1f4cd009d3bc756fd4d180efa092f071a04f5
Petra Hapalova <phapalova@nvidia.com>
2025-10-10 00:00:56 +0200

    fix hash
    Signed-off-by: Petra Hapalova <phapalova@nvidia.com>

--- src/cmake/get_nanovdb_editor.cmake
+++ src/cmake/get_nanovdb_editor.cmake
@@ -13,7 +13,7 @@
 option(NANOVDB_EDITOR_FORCE "Force rebuild of nanovdb_editor wheel" OFF)
 option(NANOVDB_EDITOR_SKIP "Skip nanovdb_editor wheel build" OFF)
 
-set(NANOVDB_EDITOR_TAG f860667e6cc2e93d62aa6f580ca34d776f4b9b79)
+set(NANOVDB_EDITOR_TAG 60e6dafbb5a4eb7b6693bb3885578b314d139dea)
 set(NANOVDB_EDITOR_VERSION 0.0.3)
 
 # If skip is set, get the latest tagged version to prevent unnecessary rebuilds each hash update
