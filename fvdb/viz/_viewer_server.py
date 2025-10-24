8911d66c3b50c21f450963cc5a95ffe5c805e73c
Francis Williams <francis@fwilliams.info>
2025-10-24 09:49:39 -0700

    try to create vulkan device on init and fail with nicer error if we can't
    Signed-off-by: Francis Williams <francis@fwilliams.info>

--- fvdb/viz/_viewer_server.py
+++ fvdb/viz/_viewer_server.py
@@ -58,6 +58,18 @@ def init(ip_address: str = "127.0.0.1", port: int = 8080, vk_device_id: int = 0,
     """
     global _viewer_server_cpp
     if _viewer_server_cpp is None:
+        import nanovdb_editor as editor
+
+        compiler = editor.Compiler()
+        compute = editor.Compute(compiler)
+        di = compute.device_interface()
+        di.create_device_manager(enable_validation=False)
+        try:
+            di.create_device(device_index=vk_device_id, enable_external_usage=False)
+        except Exception as e:
+            raise RuntimeError(
+                f"Failed to create Vulkan device with ID {vk_device_id}. You may have an incompatible version of Vulkan installed."
+            ) from e
         _viewer_server_cpp = ViewerCpp(ip_address=ip_address, port=port, device_id=vk_device_id, verbose=verbose)
     else:
         warnings.warn(
