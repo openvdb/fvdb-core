cc9aebd20c457934539b5015e78f591fbc1419f3
Jonathan Swartz <jonathan@jswartz.info>
2025-11-17 21:11:23 +1300

    test data update
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -4,4 +4,4 @@
 
 from fvdb.utils.tests import set_testing_git_tag
 
-set_testing_git_tag("fa7b9e6a7b1e08876616ed213227f92b53eff586")
+set_testing_git_tag("b3fcbaf9a336282bea92e350deda2c2c374d2caa")
