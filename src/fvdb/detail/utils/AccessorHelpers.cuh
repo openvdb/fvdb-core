f8359b7d45fd5046f83ec5697784e6682313d08d
Francis Williams <francis@fwilliams.info>
2026-01-29 15:53:25 -0800

    revert accessor helper changes
    Signed-off-by: Francis Williams <francis@fwilliams.info>

--- src/fvdb/detail/utils/AccessorHelpers.cuh
+++ src/fvdb/detail/utils/AccessorHelpers.cuh
@@ -7,9 +7,6 @@
 #include <fvdb/JaggedTensor.h>
 #include <fvdb/detail/GridBatchImpl.h>
 
-// Keep this header usable in both pure C++ and CUDA compilation units.
-// We intentionally avoid `torch/extension.h` here because it pulls in Python headers.
-#include <ATen/core/TensorAccessor.h>
 #include <torch/types.h>
 
 #include <cstdint>
@@ -20,23 +17,13 @@ namespace fvdb {
 /// @tparam ScalarType The type of the tensor
 /// @tparam DIMS The number of dimensions of the tensor
 template <typename ScalarType, int32_t DIMS>
-using TorchRAcc32 =
-#if defined(__CUDACC__) || defined(__HIPCC__)
-    at::PackedTensorAccessor32<ScalarType, DIMS, at::RestrictPtrTraits>;
-#else
-    at::PackedTensorAccessor32<ScalarType, DIMS, at::DefaultPtrTraits>;
-#endif
+using TorchRAcc32 = at::PackedTensorAccessor32<ScalarType, DIMS, at::RestrictPtrTraits>;
 
 /// @brief Shorthand for torch::PackedTensorAccessor64 with RestrictPtrTraits
 /// @tparam ScalarType The type of the tensor
 /// @tparam DIMS The number of dimensions of the tensor
 template <typename ScalarType, int32_t DIMS>
-using TorchRAcc64 =
-#if defined(__CUDACC__) || defined(__HIPCC__)
-    at::PackedTensorAccessor64<ScalarType, DIMS, at::RestrictPtrTraits>;
-#else
-    at::PackedTensorAccessor64<ScalarType, DIMS, at::DefaultPtrTraits>;
-#endif
+using TorchRAcc64 = at::PackedTensorAccessor64<ScalarType, DIMS, at::RestrictPtrTraits>;
 
 /// @brief Shorthand for a torch::TensorAccessor with DefaultPtrTraits
 /// @tparam ScalarType The type of the tensor
@@ -47,23 +34,13 @@ template <typename ScalarType, int32_t DIMS> using TorchAcc = at::TensorAccessor
 /// @tparam ScalarType The type of the tensor
 /// @tparam DIMS The number of dimensions of the tensor
 template <typename ScalarType, int32_t DIMS>
-using JaggedRAcc32 =
-#if defined(__CUDACC__) || defined(__HIPCC__)
-    fvdb::PackedJaggedAccessor32<ScalarType, DIMS, at::RestrictPtrTraits>;
-#else
-    fvdb::PackedJaggedAccessor32<ScalarType, DIMS, at::DefaultPtrTraits>;
-#endif
+using JaggedRAcc32 = fvdb::PackedJaggedAccessor32<ScalarType, DIMS, at::RestrictPtrTraits>;
 
 /// @brief Shorthand for fvdb::PackedJaggedAccessor64 with RestrictPtrTraits
 /// @tparam ScalarType The type of the tensor
 /// @tparam DIMS The number of dimensions of the tensor
 template <typename ScalarType, int32_t DIMS>
-using JaggedRAcc64 =
-#if defined(__CUDACC__) || defined(__HIPCC__)
-    fvdb::PackedJaggedAccessor64<ScalarType, DIMS, at::RestrictPtrTraits>;
-#else
-    fvdb::PackedJaggedAccessor64<ScalarType, DIMS, at::DefaultPtrTraits>;
-#endif
+using JaggedRAcc64 = fvdb::PackedJaggedAccessor64<ScalarType, DIMS, at::RestrictPtrTraits>;
 
 /// @brief Shorthand for fvdb::JaggedAccessor
 /// @tparam ScalarType The type of the tensor
@@ -80,25 +57,11 @@ using JaggedAcc = fvdb::JaggedAccessor<ScalarType, DIMS>;
 /// @return A tensor accessor (either torch::TensorAccessor or torch::PackedTensorAccessor32)
 template <c10::DeviceType DeviceTag, typename T, size_t N, typename IndexT = int32_t>
 typename std::conditional<DeviceTag == torch::kCUDA || DeviceTag == torch::kPrivateUse1,
-                          at::GenericPackedTensorAccessor<T,
-                                                          N,
-#if defined(__CUDACC__) || defined(__HIPCC__)
-                                                          at::RestrictPtrTraits,
-#else
-                                                          at::DefaultPtrTraits,
-#endif
-                                                          IndexT>,
+                          at::GenericPackedTensorAccessor<T, N, at::RestrictPtrTraits, IndexT>,
                           at::TensorAccessor<T, N>>::type
 tensorAccessor(const torch::Tensor &tensor) {
     if constexpr (DeviceTag == torch::kCUDA || DeviceTag == torch::kPrivateUse1) {
-        return tensor.generic_packed_accessor<T,
-                                              N,
-#if defined(__CUDACC__) || defined(__HIPCC__)
-                                              at::RestrictPtrTraits,
-#else
-                                              at::DefaultPtrTraits,
-#endif
-                                              IndexT>();
+        return tensor.generic_packed_accessor<T, N, at::RestrictPtrTraits, IndexT>();
     } else {
         return tensor.accessor<T, N>();
     }
