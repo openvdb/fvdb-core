8de5f4691fdd0196013a644f5d7e7f3805bce1ef
Matthew Cong <mcong@nvidia.com>
2026-04-20 12:18:33 -0700

    Remove unused SH func
    Signed-off-by: Matthew Cong <mcong@nvidia.com>

--- src/fvdb/detail/ops/gsplat/EvaluateSphericalHarmonicsForward.cu
+++ src/fvdb/detail/ops/gsplat/EvaluateSphericalHarmonicsForward.cu
@@ -432,42 +432,6 @@ dispatchEvaluateSphericalHarmonicsFwd<torch::kCPU>(const int64_t shDegreeToUse,
 }
 
 torch::Tensor
-evalSphericalHarmonics(const torch::Tensor &means,
-                       const torch::Tensor &sh0,
-                       const torch::Tensor &shN,
-                       const int64_t shDegreeToUse,
-                       const torch::Tensor &worldToCameraMatrices,
-                       const torch::Tensor &perGaussianProjectedRadii) {
-    FVDB_FUNC_RANGE();
-    const auto K              = shN.size(1) + 1;               // number of SH bases
-    const auto C              = worldToCameraMatrices.size(0); // number of cameras
-    const auto actualShDegree = shDegreeToUse < 0 ? (std::sqrt(K) - 1) : shDegreeToUse;
-    if (actualShDegree == 0) {
-        return FVDB_DISPATCH_KERNEL(sh0.device(), [&]() {
-            return dispatchEvaluateSphericalHarmonicsFwd<DeviceTag>(actualShDegree,
-                                                                    C,
-                                                                    torch::Tensor(),
-                                                                    sh0,
-                                                                    torch::Tensor(),
-                                                                    perGaussianProjectedRadii);
-        });
-    } else {
-        auto [camToWorldMatrices, info] = torch::linalg_inv_ex(worldToCameraMatrices);
-        const torch::Tensor viewDirs =
-            means.index(
-                {torch::indexing::None, torch::indexing::Slice(), torch::indexing::Slice()}) -
-            camToWorldMatrices.index({torch::indexing::Slice(),
-                                      torch::indexing::None,
-                                      torch::indexing::Slice(0, 3),
-                                      3}); // [1, N, 3] - [C, 1, 3]
-        return FVDB_DISPATCH_KERNEL(sh0.device(), [&]() {
-            return dispatchEvaluateSphericalHarmonicsFwd<DeviceTag>(
-                actualShDegree, C, viewDirs, sh0, shN, perGaussianProjectedRadii);
-        });
-    }
-}
-
-torch::Tensor
 evaluateSphericalHarmonicsFwd(const int64_t shDegreeToUse,
                               const int64_t numCameras,
                               const torch::Tensor &viewDirs,  // [C, N, 3]
