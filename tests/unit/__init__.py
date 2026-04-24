d10711c7264bfc442c59931d0b025c830919ceb6
Jonathan Swartz <jonathan@jswartz.info>
2026-04-24 22:29:21 +1200

    add tests
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -4,4 +4,4 @@
 
 from fvdb.utils.tests import set_testing_git_tag
 
-set_testing_git_tag("b3fcbaf9a336282bea92e350deda2c2c374d2caa")
+set_testing_git_tag("46e7bd447aa276c95aacb8cf6c57aa64457e2c0e")
