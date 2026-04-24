3578c3041fcf373f1873869c7a642b43eb10d69b
Jonathan Swartz <jonathan@jswartz.info>
2026-04-24 20:47:12 +1200

    undo get_nanovdb.cmake change
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- src/cmake/get_nanovdb.cmake
+++ src/cmake/get_nanovdb.cmake
@@ -1,23 +1,13 @@
 # Copyright Contributors to the OpenVDB Project
 # SPDX-License-Identifier: Apache-2.0
 
-# LOCAL BENCH REDIRECT (do not commit): picking up branchless HDDA changes from
-# the openvdb-jswartz fork for end-to-end profiling of fvdb kernels. Revert
-# this block to the GITHUB_REPOSITORY / GIT_TAG form before committing.
 CPMAddPackage(
     NAME nanovdb
-    SOURCE_DIR /home/jswartz/Development/openvdb-jswartz
+    GITHUB_REPOSITORY AcademySoftwareFoundation/openvdb
+    GIT_TAG fec59777b768eae283659c5b87e377ac31653e41
     SOURCE_SUBDIR nanovdb/nanovdb
     DOWNLOAD_ONLY YES
 )
-# Original pinned source (re-enable for upstream builds):
-# CPMAddPackage(
-#     NAME nanovdb
-#     GITHUB_REPOSITORY AcademySoftwareFoundation/openvdb
-#     GIT_TAG fec59777b768eae283659c5b87e377ac31653e41
-#     SOURCE_SUBDIR nanovdb/nanovdb
-#     DOWNLOAD_ONLY YES
-# )
 
 # NanoVDB is header only, so we don't build it. Instead, we just add the headers
 # to the include path and create an interface target.
