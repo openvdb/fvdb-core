8b0289acfdc5d347c71c7001bebd4fb2e3b049a1
Jonathan Swartz <jonathan@jswartz.info>
2026-08-18 18:22:15 +1200

    Update NanoVDB code to latest on master to pickup all needed memory resource work
    Signed-off-by: Jonathan Swartz <jonathan@jswartz.info>

--- src/cmake/get_nanovdb.cmake
+++ src/cmake/get_nanovdb.cmake
@@ -1,16 +1,10 @@
 # Copyright Contributors to the OpenVDB Project
 # SPDX-License-Identifier: Apache-2.0
 
-# TEMPORARY: pinned to the merged nanovdb injectable-memory-resource stack
-# (AcademySoftwareFoundation/openvdb PRs #2268, #2269, #2270, #2272, #2273 on
-# top of upstream master d980ad92; tracking issue #2232). fvdb relies on the
-# ResourceT seams these PRs add to route builder scratch through PyTorch's
-# caching allocator (see src/fvdb/TorchResource.h). Repoint at
-# AcademySoftwareFoundation/openvdb once the stack merges upstream.
 CPMAddPackage(
     NAME nanovdb
-    GITHUB_REPOSITORY swahtz/openvdb
-    GIT_TAG 558bfb2ead7c993f980ba18e89fee4b69991fb39
+    GITHUB_REPOSITORY AcademySoftwareFoundation/openvdb
+    GIT_TAG 63d1878f30563eb9cde07d8b433fe4868944de44
     SOURCE_SUBDIR nanovdb/nanovdb
     DOWNLOAD_ONLY YES
 )
